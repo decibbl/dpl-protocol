e1b0723bee2712f286f81bc4e5c9a58d8a5310f4
saicharanpogul <pogul1804@gmail.com>
2023-09-11 15:36:50 +0530

    Check Scripts Folder For Tests

--- tests/dpl-artists.ts
+++ tests/dpl-artists.ts
@@ -8,9 +8,5 @@ describe("dpl-artists", () => {
 
   const program = anchor.workspace.DplArtists as Program<DplArtists>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+  // all tests are in SCRIPTS folder
 });
